3cd5d95ca0f990fa678ae07d7b3f39eba54ac095
bangaffannn <bangaffannn@gmail.com>
2024-02-16 23:15:38 +0700

    Managing state

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View, Button } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Button, TextInput } from "react-native";
 import Cover from "./test/src/Cover";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.SafeView}>
       <Cover />
-    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   SafeView: {
-    backgroundColor: "white",
-    flex: 1, // Add flex: 1 to SafeAreaView to make it take up the entire screen
+    backgroundColor: "white", // Add flex: 1 to SafeAreaView to make it take up the entire screen
   },
 });
--- test/src/Cover.jsx
+++ test/src/Cover.jsx
@@ -1,15 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View, Button, TextInput } from "react-native";
+import React from "react";
+import { useState } from "react";
+import { View, StyleSheet, Text, Button, TextInput } from "react-native";
 
 export default function Cover() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your Course Goal!'></TextInput>
-        <Button title="Add Goal"></Button>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Course Goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text style={{padding: 10}}>List of Goals...</Text>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
       </View>
     </View>
   );
@@ -17,17 +32,27 @@ export default function Cover() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
     padding: 8,
-    marginRight: 8
-  }
+  },
+  goalsContainer: {
+    flex: 3, // Adjusted flex value to fill the remaining space
+  },
 });
